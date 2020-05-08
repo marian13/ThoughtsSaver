a33884396d5ec5e7076e424c2047271258c67c3c
Marian13 <mariankostyk13895@gmail.com>
2020-05-08 23:10:06 +0300

    Update TouchableWithoutFeedbackView, use app View instead of native View, add propTypes and testID

--- src/components/TouchableWithoutFeedbackView/index.js
+++ src/components/TouchableWithoutFeedbackView/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { TouchableWithoutFeedback, View } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native';
 
-const TouchableWithoutFeedbackView = ({ children, onPress, style }) => (
-  <TouchableWithoutFeedback onPress={onPress}>
+import View from '@components/View';
+
+import { createPropTypes, PropTypes } from '@utils/propTypes';
+
+const TouchableWithoutFeedbackView = ({ testID, children, onPress, style }) => (
+  <TouchableWithoutFeedback testID={testID} onPress={onPress}>
     <View style={style}>
       {children}
     </View>
   </TouchableWithoutFeedback>
 );
+
+[TouchableWithoutFeedbackView.propTypes, TouchableWithoutFeedbackView.defaultProps] = createPropTypes({
+  testID: PropTypes.string,
+  children: PropTypes.node,
+  onPress: PropTypes.func,
+  style: View.propTypes.style
+});
 
 export default TouchableWithoutFeedbackView;
