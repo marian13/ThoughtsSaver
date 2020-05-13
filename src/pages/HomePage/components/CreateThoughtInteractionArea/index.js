611b879929c33878f0b148a78fec2fbd6e334e4a
Marian13 <mariankostyk13895@gmail.com>
2020-05-14 02:17:06 +0300

    General Thought Options Button

--- src/pages/HomePage/components/CreateThoughtInteractionArea/index.js
+++ src/pages/HomePage/components/CreateThoughtInteractionArea/index.js
@@ -1,38 +1,71 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 
 import InteractionRow from '@components/InteractionRow';
 import IconButton from '@components/IconButton';
+import OverlayMenu from '@components/OverlayMenu';
 import TextInput from '@components/TextInput';
+import View from '@components/View';
+
+import {
+  CREATE_THOUGHT_TEXT_INPUT,
+  CREATE_THOUGHT_BUTTON,
+  GENERAL_THOUGHT_OPTIONS_BUTTON
+} from '@pages/HomePage/constants/identifiers';
+
+const styles = StyleSheet.create({
+  overlayMenuContainer: {
+    paddingLeft: 10
+  }
+});
 
 const CreateThoughtInteractionArea = ({
   isCreateThoughtTextInputFocused,
+  isGeneralThoughtOptionsMenuVisible,
   thoughtInCreateDraftText,
   onCreateThoughtTextInputChange,
   onCreateThoughtTextInputFocus,
   onCreateThoughtTextInputBlur,
-  onCreateThoughtButtonPress
+  onCreateThoughtButtonPress,
+  onGeneralThoughtOptionsButtonPress,
+  onGeneralThoughtOptionsMenuBackdropPress
 }) => (
-  <InteractionRow>
-    <IconButton
-      iconName="emoticon-outline"
-      iconType="material-community"
-    />
+  <View avoidKeyboard>
+    <InteractionRow>
+      <OverlayMenu
+        isVisible={isGeneralThoughtOptionsMenuVisible}
+        onContentPress={onGeneralThoughtOptionsButtonPress}
+        onBackdropPress={onGeneralThoughtOptionsMenuBackdropPress}
+        renderContent={() => <IconButton testID={GENERAL_THOUGHT_OPTIONS_BUTTON} iconName="more-vert" />}
+        height={100}
+        containerStyle={styles.overlayMenuContainer}
+      />
+    </InteractionRow>
 
-    <TextInput
-      isFocused={isCreateThoughtTextInputFocused}
-      autoResise
-      text={thoughtInCreateDraftText}
-      placeholder="Save a new thought..."
-      onFocus={onCreateThoughtTextInputFocus}
-      onBlur={onCreateThoughtTextInputBlur}
-      onTextChange={onCreateThoughtTextInputChange}
-    />
+    <InteractionRow>
+      <IconButton
+        iconName="emoticon-outline"
+        iconType="material-community"
+      />
 
-    <IconButton
-      iconName="send"
-      onPress={onCreateThoughtButtonPress}
-    />
-  </InteractionRow>
+      <TextInput
+        testID={CREATE_THOUGHT_TEXT_INPUT}
+        isFocused={isCreateThoughtTextInputFocused}
+        autoResise
+        text={thoughtInCreateDraftText}
+        placeholder="Save a new thought..."
+        onFocus={onCreateThoughtTextInputFocus}
+        onBlur={onCreateThoughtTextInputBlur}
+        onTextChange={onCreateThoughtTextInputChange}
+      />
+
+      <IconButton
+        testID={CREATE_THOUGHT_BUTTON}
+        iconName="send"
+        onPress={onCreateThoughtButtonPress}
+      />
+    </InteractionRow>
+  </View>
 );
 
 export default CreateThoughtInteractionArea;
--- src/pages/HomePage/containers/CreateThoughtInteractionAreaContainer/index.js
+++ src/pages/HomePage/containers/CreateThoughtInteractionAreaContainer/index.js
@@ -4,30 +4,41 @@ import { useDispatch, useSelector } from 'react-redux';
 import CreateThoughtInteractionArea from '@pages/HomePage/components/CreateThoughtInteractionArea';
 
 import { isFocusedSelector, focus, blur } from '@modules/FocusesModule/slice';
+import { isVisibleSelector, show, hide } from '@modules/VisibilitiesModule/slice';
+
 import { thoughtInCreateDraftTextSelector, changeThoughtInCreateDraftText } from '@pages/HomePage/slice';
 
 import createThoughtThunk from '@pages/HomePage/thunks/createThoughtThunk';
 
+import { CREATE_THOUGHT_TEXT_INPUT, GENERAL_THOUGHT_OPTIONS_MENU } from '@pages/HomePage/constants/identifiers';
+
 const CreateThoughtInteractionAreaContainer = () => {
   const dispatch = useDispatch();
 
-  const isCreateThoughtTextInputFocused = useSelector(isFocusedSelector('createThoughtTextInput'));
+  const isCreateThoughtTextInputFocused = useSelector(isFocusedSelector(CREATE_THOUGHT_TEXT_INPUT));
+  const isGeneralThoughtOptionsMenuVisible = useSelector(isVisibleSelector(GENERAL_THOUGHT_OPTIONS_MENU));
   const thoughtInCreateDraftText = useSelector(thoughtInCreateDraftTextSelector);
 
   const handleCreateThoughtTextInputChange = text => dispatch(changeThoughtInCreateDraftText({ text }));
-  const handleCreateThoughtTextInputFocus = () => dispatch(focus('createThoughtTextInput'));
-  const handleCreateThoughtTextInputBlur = () => dispatch(blur('createThoughtTextInput'));
+  const handleCreateThoughtTextInputFocus = () => dispatch(focus(CREATE_THOUGHT_TEXT_INPUT));
+  const handleCreateThoughtTextInputBlur = () => dispatch(blur(CREATE_THOUGHT_TEXT_INPUT));
 
   const handleCreateThoughtButtonPress = () => dispatch(createThoughtThunk());
+
+  const handleGeneralThoughtOptionsButtonPress = () => dispatch(show(GENERAL_THOUGHT_OPTIONS_MENU));
+  const handleGeneralThoughtOptionsMenuBackdropPress = () => dispatch(hide(GENERAL_THOUGHT_OPTIONS_MENU));
 
   return (
     <CreateThoughtInteractionArea
       isCreateThoughtTextInputFocused={isCreateThoughtTextInputFocused}
+      isGeneralThoughtOptionsMenuVisible={isGeneralThoughtOptionsMenuVisible}
       thoughtInCreateDraftText={thoughtInCreateDraftText}
       onCreateThoughtTextInputChange={handleCreateThoughtTextInputChange}
       onCreateThoughtTextInputFocus={handleCreateThoughtTextInputFocus}
       onCreateThoughtTextInputBlur={handleCreateThoughtTextInputBlur}
       onCreateThoughtButtonPress={handleCreateThoughtButtonPress}
+      onGeneralThoughtOptionsButtonPress={handleGeneralThoughtOptionsButtonPress}
+      onGeneralThoughtOptionsMenuBackdropPress={handleGeneralThoughtOptionsMenuBackdropPress}
     />
   );
 };
