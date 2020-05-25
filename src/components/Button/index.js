f4c00928ccecad41b389b17ef8db953cdfe0fe66
Marian13 <mariankostyk13895@gmail.com>
2020-05-26 01:03:05 +0300

    Initial Button

---
+++ src/components/Button/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Button as NativeElementsButton } from 'react-native-elements';
+
+import Icon from '@components/Icon';
+
+import { fontSizes } from '@constants/fonts';
+import { ifProp } from '@utils/props';
+import { createPropTypes, PropTypes, ViewPropTypes } from '@utils/propTypes';
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: fontSizes.xLarge
+  },
+  iconContainer: {
+    paddingVertical: 5
+  }
+});
+
+const outlineProp = { type: 'outline' };
+const iconProp = ({ iconName, iconType, containerStyle }) => ({
+  icon: (
+    <Icon
+      name={iconName}
+      type={iconType}
+      containerStyle={containerStyle}
+    />
+  )
+});
+
+const Button = ({
+  outline,
+  iconName,
+  iconType,
+  text,
+  style
+}) => (
+  <NativeElementsButton
+    raised
+    {...ifProp(outline && outlineProp)}
+    {...ifProp(iconName && iconProp({ iconName, iconType, containerStyle: styles.iconContainer }))}
+    title={text}
+    containerStyle={style}
+    titleStyle={styles.title}
+  />
+);
+
+[Button.propTypes, Button.defaultProps] = createPropTypes({
+  outline: PropTypes.bool,
+  iconName: PropTypes.string,
+  iconType: PropTypes.string,
+  text: [PropTypes.string, 'OK'],
+  onPress: PropTypes.func,
+  style: ViewPropTypes.style
+});
+
+export default Button;
--- src/constants/fonts.js
+++ src/constants/fonts.js
@@ -2,7 +2,8 @@
 const fontSizes = {
   small: 12,
   medium: 14,
-  large: 20
+  large: 20,
+  xLarge: 24
 };
 
 export { fontSizes };
