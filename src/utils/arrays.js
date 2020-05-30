3e418b55546a8a39b1785c0ce4c625e3fe22b3b5
Marian13 <mariankostyk13895@gmail.com>
2020-05-30 14:15:39 +0300

    Add, Remove, AddOrRemove array utils
    Signed-off-by: Marian13 <mariankostyk13895@gmail.com>

--- src/utils/arrays.js
+++ src/utils/arrays.js
@@ -1,8 +1,38 @@
 // https://api.rubyonrails.org/classes/Array.html#method-c-wrap
-export const wrapArray = object => {
+export const wrap = object => {
   if (object === undefined) return [];
   if (object === null) return [];
   if (Array.isArray(object)) return object;
 
   return [object];
+};
+
+export const add = (array, elementToAdd) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === elementToAdd) {
+      return array;
+    }
+  }
+
+  return [...array, elementToAdd];
+};
+
+export const remove = (array, elementToRemove) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === elementToRemove) {
+      return array.slice(0, i).concat(array.slice(i + 1, array.length));
+    }
+  }
+
+  return array;
+};
+
+export const addOrRemove = (array, elementToAddOrRemove) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === elementToAddOrRemove) {
+      return array.slice(0, i).concat(array.slice(i + 1, array.length));
+    }
+  }
+
+  return [...array, elementToAdd];
 };
