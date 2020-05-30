b714538d878b64471589a2361b429eaa5fd869df
Marian13 <mariankostyk13895@gmail.com>
2020-05-30 23:10:13 +0300

    Array#empty specs

---
+++ spec/utils/arrays/empty.spec.js
@@ -0,0 +1,21 @@
+import '@specHelper';
+
+import { empty } from '@utils/arrays';
+
+describe('empty', () => {
+  context('when array is empty', () => {
+    it('returns true', () => {
+      const array = [];
+
+      expect(empty(array)).toEqual(true);
+    });
+  });
+
+  context('when array is NOT empty', () => {
+    it('returns false', () => {
+      const array = [1];
+
+      expect(empty(array)).toEqual(false);
+    });
+  });
+});
