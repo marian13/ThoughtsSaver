d9b36b512486de873621cbb11b4c1eca9f3e9345
Marian13 <mariankostyk13895@gmail.com>
2020-05-31 18:10:29 +0300

    Allow any to receive function and specs

--- spec/utils/arrays/any.spec.js
+++ spec/utils/arrays/any.spec.js
@@ -1,21 +1,53 @@
 import '@specHelper';
 
 import { any } from '@utils/arrays';
+import { AnyFunctionError } from '@utils/arrays/any';
 
 describe('any', () => {
-  context('when array is empty', () => {
-    it('returns false', () => {
-      const array = [];
+  context('when no function passed', () => {
+    context('and array is empty', () => {
+      it('returns false', () => {
+        const array = [];
 
-      expect(any(array)).toEqual(false);
+        expect(any(array)).toEqual(false);
+      });
+    });
+
+    context('and array is NOT empty', () => {
+      it('returns false', () => {
+        const array = [1];
+
+        expect(any(array)).toEqual(true);
+      });
     });
   });
 
-  context('when array is NOT empty', () => {
-    it('returns false', () => {
-      const array = [1];
+  context('when function passed', () => {
+    context('and function is not function', () => {
+      it('throws AnyFunctionError', () => {
+        const array = [];
+        const func = 45;
 
-      expect(any(array)).toEqual(true);
+        expect(() =>any(array, func)).toThrowError(new AnyFunctionError('Function passed to any is not a function.'));
+      });
+    });
+
+    context('no element for function returns truthy value', () => {
+      it('returns false', () => {
+        const array = [1, 2, 3, 4, 5];
+        const func = number => number > 6;
+
+        expect(any(array, func)).toEqual(false);
+      });
+    });
+
+    context('at least one element for function return truthy value', () => {
+      it('returns false', () => {
+        const array = [1, 2, 3, 4, 5];
+        const func = number => number > 3;
+
+        expect(any(array, func)).toEqual(true);
+      });
     });
   });
 });
--- src/utils/arrays/any.js
+++ src/utils/arrays/any.js
@@ -1,3 +1,19 @@
-const any = array => array.length > 0;
+import isFunction from '@utils/functions/isFunction';
+import toBool from '@utils/booleans/toBool';
+import isUndefined from '@utils/undefined/isUndefined';
+
+export class AnyFunctionError extends Error {};
+
+const throwAnyFunctionError = () => {
+  throw new AnyFunctionError('Function passed to any is not a function.');
+};
+
+const any = (array, anyFunction) => {
+  if (isFunction(anyFunction)) return toBool(array.find(anyFunction));
+
+  if (!isUndefined(anyFunction)) throwAnyFunctionError();
+
+  return array.length > 0;
+};
 
 export default any;
