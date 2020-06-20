02d6b935d02f9d4b76ca6d8e5b14243e22f8ff60
Marian13 <mariankostyk13895@gmail.com>
2020-06-20 17:55:53 +0300

    Delete container for ThoughtMenuOptions

--- src/pages/HomePage/components/Thought/ThoughtMenuOptions/index.js
+++ src/pages/HomePage/components/Thought/ThoughtMenuOptions/index.js
@@ -1,23 +1,34 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import OverlayMenuOption from '@components/OverlayMenuOption';
 
-const ThoughtMenuOptions = ({ onEditTextPress, onAddTagsPress }) => (
-  <>
-    <OverlayMenuOption
-      iconName="playlist-edit"
-      iconType="material-community"
-      text="Edit Text"
-      onPress={onEditTextPress}
-    />
+import startEditThoughtTextThunk from '@pages/HomePage/thunks/startEditThoughtTextThunk';
+import startEditThoughtTagsThunk from '@pages/HomePage/thunks/startEditThoughtTagsThunk';
 
-    <OverlayMenuOption
-      iconName="tag-plus"
-      iconType="material-community"
-      text="Add Tags"
-      onPress={onAddTagsPress}
-    />
-  </>
-);
+const ThoughtMenuOptions = ({ thought }) => {
+  const dispatch = useDispatch();
+
+  const handleEditTextPress = () => dispatch(startEditThoughtTextThunk({ thought }));
+  const handleAddTagsPress = () => dispatch(startEditThoughtTagsThunk({ thought }));
+
+  return (
+    <>
+      <OverlayMenuOption
+        iconName="playlist-edit"
+        iconType="material-community"
+        text="Edit Text"
+        onPress={handleEditTextPress}
+      />
+
+      <OverlayMenuOption
+        iconName="tag-plus"
+        iconType="material-community"
+        text="Add Tags"
+        onPress={handleAddTagsPress}
+      />
+    </>
+  );
+};
 
 export default ThoughtMenuOptions;
