ab59300f9767744cdbe3783e98a8866839210c64
Marian13 <mariankostyk13895@gmail.com>
2020-06-20 20:04:11 +0300

    Update HomePage

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 
 import Page from '@components/Page';
-import Header from '@pages/HomePage/components/Header';
-import AddThoughtTagsModal from '@modals/AddThoughtTagsModal';
 
+import Header from '@pages/HomePage/components/Header';
 import ThoughtStream from '@pages/HomePage/components/ThoughtStream';
-import ThoughtInteractionAreaContainer from '@pages/HomePage/containers/ThoughtInteractionAreaContainer';
+import ThoughtInteractionArea from '@pages/HomePage/components/ThoughtInteractionArea';
+
+import AddThoughtTagsModal from '@modals/AddThoughtTagsModal';
 
 const styles = StyleSheet.create({
   page: {
@@ -17,10 +18,8 @@ const styles = StyleSheet.create({
 const HomePage = () => (
   <Page style={styles.page}>
     <Header />
-
     <ThoughtStream />
-
-    <ThoughtInteractionAreaContainer />
+    <ThoughtInteractionArea />
 
     <AddThoughtTagsModal />
   </Page>
