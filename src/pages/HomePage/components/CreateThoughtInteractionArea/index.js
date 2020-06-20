cf9d1248cd6b70dcc00aac642bdf12bb0a35996c
Marian13 <mariankostyk13895@gmail.com>
2020-06-20 19:20:35 +0300

    Update CreateThoughtInteractionArea

--- src/pages/HomePage/components/CreateThoughtInteractionArea/index.js
+++ src/pages/HomePage/components/CreateThoughtInteractionArea/index.js
@@ -1,101 +1,47 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 
-import { isEmpty } from 'lodash';
-
-import Icon from '@components/Icon';
-import IconButton from '@components/IconButton';
 import InteractionRow from '@components/InteractionRow';
-import OverlayMenu from '@components/OverlayMenu';
-import TextInput from '@components/TextInput';
 import View from '@components/View';
 
-import EmojiPickerCollapse from '@pages/HomePage/components/EmojiPickerCollapse';
+import ThoughtTextInput from '@pages/HomePage/components/ThoughtTextInput';
+import SaveThoughtButton from '@pages/HomePage/components/SaveThoughtButton';
+
+import ToggleModeOptionsCollapseButton from '@pages/HomePage/components/ToggleModeOptionsCollapseButton';
 import ToggleEmojiPickerCollapseButton from '@pages/HomePage/components/ToggleEmojiPickerCollapseButton';
+import AttachFileToThoughtButton from '@pages/HomePage/components/AttachFileToThoughtButton';
+import ToggleAdvancedSaveThoughtModalButton from '@pages/HomePage/components/ToggleAdvancedSaveThoughtModalButton';
 
-import {
-  CREATE_THOUGHT_TEXT_INPUT,
-  CREATE_THOUGHT_BUTTON,
-  GENERAL_THOUGHT_OPTIONS_BUTTON
-} from '@pages/HomePage/constants/identifiers';
+import AttachRecordingToThoughtButton from '@pages/HomePage/components/AttachRecordingToThoughtButton';
+import AttachPhotoToThoughtButton from '@pages/HomePage/components/AttachPhotoToThoughtButton';
+import ShowGeneralThoughtOptionsButton from '@pages/HomePage/components/ShowGeneralThoughtOptionsButton';
 
-const styles = StyleSheet.create({
-  textInputContainerStyle: {
-    paddingLeft: 15
-  }
-});
+import EmojiPickerCollapse from '@pages/HomePage/components/EmojiPickerCollapse';
+import AdvancedSaveThoughtModal from '@pages/HomePage/components/AdvancedSaveThoughtModal';
 
-const CreateThoughtInteractionArea = ({
-  isCreateThoughtTextInputFocused,
-  isGeneralThoughtOptionsMenuVisible,
-  isEmojiPickerCollapseVisible,
-  thoughtInCreateDraftText,
-  onCreateThoughtTextInputChange,
-  onCreateThoughtTextInputFocus,
-  onCreateThoughtTextInputBlur,
-  onCreateThoughtButtonPress,
-  onGeneralThoughtOptionsButtonPress,
-  onGeneralThoughtOptionsMenuBackdropPress,
-  onShowEmojiPickerCollapseButtonPress,
-  onHideEmojiPickerCollapseButtonPress,
-  onEmojiPick,
-  GeneralThoughtMenuOptionsContainer
-}) => (
+const CreateThoughtInteractionArea = () => (
   <View avoidKeyboard>
     <InteractionRow>
-      <TextInput
-        testID={CREATE_THOUGHT_TEXT_INPUT}
-        isFocused={isCreateThoughtTextInputFocused}
-        autoResise
-        text={thoughtInCreateDraftText}
-        placeholder="Save a new thought..."
-        onFocus={onCreateThoughtTextInputFocus}
-        onBlur={onCreateThoughtTextInputBlur}
-        onTextChange={onCreateThoughtTextInputChange}
-        containerStyle={styles.textInputContainerStyle}
-      />
-
-      <IconButton
-        testID={CREATE_THOUGHT_BUTTON}
-        disabled={isEmpty(thoughtInCreateDraftText)}
-        iconName="send"
-        onPress={onCreateThoughtButtonPress}
-      />
+      <ThoughtTextInput />
+      <SaveThoughtButton />
     </InteractionRow>
 
     <InteractionRow>
       <View horizontal>
-        <IconButton iconName="keyboard-arrow-down" />
-
-        <ToggleEmojiPickerCollapseButton
-          isEmojiPickerCollapseVisible={isEmojiPickerCollapseVisible}
-          onShowPress={onShowEmojiPickerCollapseButtonPress}
-          onHidePress={onHideEmojiPickerCollapseButtonPress}
-        />
-
-        <IconButton iconName="attach-file" />
+        <ToggleModeOptionsCollapseButton />
+        <ToggleEmojiPickerCollapseButton />
+        <AttachFileToThoughtButton />
+        <ToggleAdvancedSaveThoughtModalButton />
       </View>
 
       <View horizontal>
-        <IconButton iconName="mic" />
-        <IconButton iconName="photo-camera" />
-
-        <OverlayMenu
-          isVisible={isGeneralThoughtOptionsMenuVisible}
-          onContentPress={onGeneralThoughtOptionsButtonPress}
-          onBackdropPress={onGeneralThoughtOptionsMenuBackdropPress}
-          renderContent={() => <Icon testID={GENERAL_THOUGHT_OPTIONS_BUTTON} name="more-vert" />}
-          renderOptions={() => <GeneralThoughtMenuOptionsContainer />}
-          height={100}
-          width="55%"
-        />
+        <AttachRecordingToThoughtButton />
+        <AttachPhotoToThoughtButton />
+        <ShowGeneralThoughtOptionsButton />
       </View>
     </InteractionRow>
 
-    <EmojiPickerCollapse
-      isVisible={isEmojiPickerCollapseVisible}
-      onEmojiPick={onEmojiPick}
-    />
+    <EmojiPickerCollapse />
+    <AdvancedSaveThoughtModal />
   </View>
 );
 
