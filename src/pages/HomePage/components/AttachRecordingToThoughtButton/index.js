ca79bf109a50132108b3160ef1d0bc3e124707e1
Marian13 <mariankostyk13895@gmail.com>
2020-06-20 19:15:11 +0300

    AttachRecordingToThoughtButton

---
+++ src/pages/HomePage/components/AttachRecordingToThoughtButton/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import IconButton from '@components/IconButton';
+
+const AttachRecordingToThoughtButton = () => {
+  return <IconButton iconName="mic" />;
+};
+
+export default AttachRecordingToThoughtButton;
