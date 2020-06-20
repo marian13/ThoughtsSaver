f498a0d1e6b756805d8623911617b7ea06f3265f
Marian13 <mariankostyk13895@gmail.com>
2020-06-20 19:14:43 +0300

    AttachPhotoToThoughtButton

---
+++ src/pages/HomePage/components/AttachPhotoToThoughtButton/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import IconButton from '@components/IconButton';
+
+const AttachPhotoToThoughtButton = () => {
+  return <IconButton iconName="photo-camera" />;
+};
+
+export default AttachPhotoToThoughtButton;
