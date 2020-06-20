2c42795db7c9e585cfab4e7dcc0e962d356f4ffe
Marian13 <mariankostyk13895@gmail.com>
2020-06-20 19:13:48 +0300

    AttachFileToThoughtButton

---
+++ src/pages/HomePage/components/AttachFileToThoughtButton/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import IconButton from '@components/IconButton';
+
+const AttachFileToThoughtButton = () => {
+  return <IconButton iconName="attach-file" />;
+};
+
+export default AttachFileToThoughtButton;
