b60dba3c5b26d84aeac295c74e39136e9e15fc56
Marian13 <mariankostyk13895@gmail.com>
2020-06-20 19:19:10 +0300

    src/pages/HomePage/components/ToggleEmojiPickerCollapseButton/index.js

--- src/pages/HomePage/components/ToggleEmojiPickerCollapseButton/index.js
+++ src/pages/HomePage/components/ToggleEmojiPickerCollapseButton/index.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import IconButton from '@components/IconButton';
 
-import { TOGGLE_EMOJI_PICKER_BUTTON } from '@pages/HomePage/constants/identifiers';
+import { isVisibleSelector, show, hide } from '@modules/VisibilitiesModule/slice';
+
+import { EMOJI_PICKER_COLLAPSE, TOGGLE_EMOJI_PICKER_COLLAPSE_BUTTON } from '@pages/HomePage/constants/identifiers';
 
 import { ifProp } from '@utils/props';
 
-const ToggleEmojiPickerCollapseButton = ({
-  isEmojiPickerCollapseVisible,
-  onShowPress,
-  onHidePress
-}) => (
-  <IconButton
-    testID={TOGGLE_EMOJI_PICKER_BUTTON}
-    iconName="tag-faces"
-    onPress={() => isEmojiPickerCollapseVisible ? onHidePress() : onShowPress()}
-    {...ifProp(isEmojiPickerCollapseVisible && { style: { transform: [{ rotate: '180deg'}] } })}
-  />
-);
+const ToggleEmojiPickerCollapseButton = () => {
+  const dispatch = useDispatch();
+
+  const isCollapseVisible = useSelector(isVisibleSelector(EMOJI_PICKER_COLLAPSE));
+
+  const handleButtonPress = () => (
+    isCollapseVisible ? dispatch(hide(EMOJI_PICKER_COLLAPSE)) : dispatch(show(EMOJI_PICKER_COLLAPSE))
+  );
+
+  return (
+    <IconButton
+      testID={TOGGLE_EMOJI_PICKER_COLLAPSE_BUTTON}
+      iconName="tag-faces"
+      onPress={handleButtonPress}
+      {...ifProp(isCollapseVisible && { style: { transform: [{ rotate: '180deg'}] } })}
+    />
+  );
+};
 
 export default ToggleEmojiPickerCollapseButton;
