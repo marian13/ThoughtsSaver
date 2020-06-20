10034866495adc1bc040180c533917712bbd527d
Marian13 <mariankostyk13895@gmail.com>
2020-06-20 18:33:48 +0300

    Save Thought Button

---
+++ src/pages/HomePage/components/SaveThoughtButton/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import IconButton from '@components/IconButton';
+
+import { thoughtTextSelector } from '@pages/HomePage/slice';
+
+import saveThoughtThunk from '@pages/HomePage/thunks/saveThoughtThunk';
+
+import { toBool } from '@utils/booleans';
+
+import { SAVE_THOUGHT_BUTTON } from '@pages/HomePage/constants/identifiers';
+
+const SaveThoughtButton = () => {
+  const dispatch = useDispatch();
+
+  const thoughtText = useSelector(thoughtTextSelector);
+
+  const handleButtonPress = () => dispatch(saveThoughtThunk());
+
+  return (
+    <IconButton
+      testID={SAVE_THOUGHT_BUTTON}
+      disabled={toBool(thoughtText)}
+      iconName="send"
+      onPress={handleButtonPress}
+    />
+  );
+};
+
+export default SaveThoughtButton;
