d930247940dbb5c0baeebd13696887141eaa81bb
Marian13 <mariankostyk13895@gmail.com>
2020-06-20 17:56:31 +0300

    Delete container for Thought

--- src/pages/HomePage/components/Thought/index.js
+++ src/pages/HomePage/components/Thought/index.js
@@ -1,35 +1,42 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-
-import ThoughtContent from './ThoughtContent';
+import { useDispatch, useSelector } from 'react-redux';
 
 import OverlayMenu from '@components/OverlayMenu';
+
+import ThoughtContent from './ThoughtContent';
+import ThoughtMenuOptions from './ThoughtMenuOptions';
+
+import { isVisibleSelector, show, hide } from '@modules/VisibilitiesModule/slice';
+
+import { generateThoughtTestID } from '@utils/generators';
 
 const styles = StyleSheet.create({
   overlayMenuContainer: {
     paddingLeft: 10
   }
-})
+});
 
-const Thought = ({
-  testID,
-  isMenuVisible,
-  thought,
-  onContentPress,
-  onMenuBackdropPress,
-  ThoughtMenuOptionsContainer
-}) => (
-  <OverlayMenu
-    isVisible={isMenuVisible}
-    onContentPress={onContentPress}
-    onBackdropPress={onMenuBackdropPress}
-    renderContent={() => <ThoughtContent testID={testID} {...thought} />}
-    renderOptions={() => <ThoughtMenuOptionsContainer thought={thought} />}
-    height={100}
-    width="50%"
-    containerStyle={styles.overlayMenuContainer}
-  />
-);
+const Thought = ({ thought }) => {
+  const dispatch = useDispatch();
+  const isMenuVisible = useSelector(isVisibleSelector(`thoughtMenu${thought.id}`));
+
+  const handleContentPress = () => dispatch(show(`thoughtMenu${thought.id}`));
+  const handleMenuBackdropPress = () => dispatch(hide(`thoughtMenu${thought.id}`));
+
+  return (
+    <OverlayMenu
+      isVisible={isMenuVisible}
+      onContentPress={handleContentPress}
+      onBackdropPress={handleMenuBackdropPress}
+      renderContent={() => <ThoughtContent testID={generateThoughtTestID(thought.text)} {...thought} />}
+      renderOptions={() => <ThoughtMenuOptions thought={thought} />}
+      height={100}
+      width="50%"
+      containerStyle={styles.overlayMenuContainer}
+    />
+  );
+};
 
 export default Thought;
 
