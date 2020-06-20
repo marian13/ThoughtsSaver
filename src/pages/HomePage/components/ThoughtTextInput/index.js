f42a37178a50c2d1040c91136d4b80bc00c05d9b
Marian13 <mariankostyk13895@gmail.com>
2020-06-20 18:22:36 +0300

    Extract ThoughtTextInput

---
+++ src/pages/HomePage/components/ThoughtTextInput/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+
+import TextInput from '@components/TextInput';
+
+import { isFocusedSelector, focus, blur } from '@modules/FocusesModule/slice';
+import { thoughtTextSelector, changeThoughtText } from '@pages/HomePage/slice';
+
+import { THOUGHT_TEXT_INPUT } from '@pages/HomePage/constants/identifiers';
+
+const styles = StyleSheet.create({
+  textInputContainerStyle: {
+    paddingLeft: 15
+  }
+});
+
+const ThoughtTextInput = () => {
+  const dispatch = useDispatch();
+
+  const isInputFocused = useSelector(isFocusedSelector(THOUGHT_TEXT_INPUT));
+
+  const thoughtText = useSelector(thoughtTextSelector);
+
+  const handleInputChange = text => dispatch(changeThoughtText({ text }));
+  const handleInputFocus = () => dispatch(focus(THOUGHT_TEXT_INPUT));
+  const handleInputBlur = () => dispatch(blur(THOUGHT_TEXT_INPUT));
+
+  return (
+    <TextInput
+      testID={THOUGHT_TEXT_INPUT}
+      isFocused={isInputFocused}
+      autoResise
+      text={thoughtText}
+      placeholder="Save a thought..."
+      onTextChange={handleInputChange}
+      onFocus={handleInputFocus}
+      onBlur={handleInputBlur}
+      containerStyle={styles.textInputContainerStyle}
+    />
+  );
+};
+
+export default ThoughtTextInput;
