6bdc7304783a8da602c49995549f5240b228e52f
Marian13 <mariankostyk13895@gmail.com>
2020-06-27 16:07:27 +0300

    Change default Entry point

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// https://docs.expo.io/versions/latest/sdk/register-root-component/
+import registerRootComponent from 'expo/build/launch/registerRootComponent';
+
+import React from 'react';
+
+import './globals';
+
+import ReduxProvider from './redux/ReduxProvider';
+import NavigationProvider from './navigation/NavigationProvider';
+
+const App = () => (
+  <ReduxProvider>
+    <NavigationProvider />
+  </ReduxProvider>
+);
+
+registerRootComponent(App);
