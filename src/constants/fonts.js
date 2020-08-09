46299fe77b2c8e848e8868ab669253ce4f5533c1
Marian13 <mariankostyk13895@gmail.com>
2020-08-10 00:42:08 +0300

    Heading font sizes

--- src/constants/fonts.js
+++ src/constants/fonts.js
@@ -3,7 +3,14 @@ const fontSizes = {
   small: 12,
   medium: 14,
   large: 20,
-  xLarge: 24
+  xLarge: 24,
+
+  heading1: 48,
+  heading2: 40,
+  heading3: 36,
+  heading4: 32,
+  heading5: 28,
+  heading6: 24
 };
 
 export { fontSizes };
