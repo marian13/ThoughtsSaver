aa87c9bc1b815aec3d586103d54f8284690f7cd6
Marian13 <mariankostyk13895@gmail.com>
2020-08-10 00:44:24 +0300

    scrollToBottomOnChange for ScrollView

--- src/components/ScrollView/index.js
+++ src/components/ScrollView/index.js
@@ -1,3 +1,12 @@
-import { ScrollView } from 'react-native';
+import React, { useRef } from 'react';
+import { ScrollView as NativeScrollView } from 'react-native';
+
+const ScrollView = ({ scrollToBottomOnChange, ...rest }) => {
+  const scrollViewRef = useRef(null);
+
+  const handleContentSizeChange = () => scrollToBottomOnChange && scrollViewRef.current.scrollToEnd();
+
+  return <NativeScrollView onContentSizeChange={handleContentSizeChange} {...rest} />;
+};
 
 export default ScrollView;
