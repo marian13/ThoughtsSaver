b32fcc835348a45fd57ea502f34740c08c71ebed
Marian13 <mariankostyk13895@gmail.com>
2020-08-11 00:39:36 +0300

    Scroll Throught Preview to bottom when its text end changed

--- src/pages/HomePage/components/ThoughtStream/components/ThoughtPreview/index.js
+++ src/pages/HomePage/components/ThoughtStream/components/ThoughtPreview/index.js
@@ -4,6 +4,8 @@ import { StyleSheet } from 'react-native';
 import ScrollView from '~/components/ScrollView';
 import MarkdownView from '~/components/MarkdownView';
 
+import { useIsStringEndChanged } from '~/utils/hooks';
+
 const styles = StyleSheet.create({
   view: {
     paddingLeft: 15,
@@ -11,12 +13,16 @@ const styles = StyleSheet.create({
   }
 });
 
-const ThoughtPreview = ({ thought }) => (
-  <ScrollView>
-    <MarkdownView style={styles.view}>
-      {thought.text}
-    </MarkdownView>
-  </ScrollView>
-);
+const ThoughtPreview = ({ thought }) => {
+  const isTextEndChanged = useIsStringEndChanged(thought.text);
+
+  return (
+    <ScrollView scrollToBottomOnChange={isTextEndChanged}>
+      <MarkdownView style={styles.view}>
+        {thought.text}
+      </MarkdownView>
+    </ScrollView>
+  );
+};
 
 export default ThoughtPreview;
