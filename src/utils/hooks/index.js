2cfc829171d8d24723524318cfab29929dd7df28
Marian13 <mariankostyk13895@gmail.com>
2020-08-11 00:07:36 +0300

    New index file for hooks

---
+++ src/utils/hooks/index.js
@@ -0,0 +1,19 @@
+import useComponentSize from '~/utils/hooks/useComponentSize';
+import useFocus from '~/utils/hooks/useFocus';
+import useIsStringEndChanged from '~/utils/hooks/useIsStringEndChanged';
+import useKeyboardHeight from '~/utils/hooks/useKeyboardHeight';
+import useMountEffect from '~/utils/hooks/useMountEffect';
+import usePreviousValue from '~/utils/hooks/usePreviousValue';
+import useScrollToBottom from '~/utils/hooks/useScrollToBottom';
+import useWindowHeight from '~/utils/hooks/useWindowHeight';
+
+export {
+  useComponentSize,
+  useFocus,
+  useKeyboardHeight,
+  useIsStringEndChanged,
+  useMountEffect,
+  usePreviousValue,
+  useScrollToBottom,
+  useWindowHeight
+};
